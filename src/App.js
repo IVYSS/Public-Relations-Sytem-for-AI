23bebfeefe22315eaec1739eb2576cc8206f0535
unknown <laxezice@gmail.com>
2021-08-18 20:53:59 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Switch, BrowserRouter, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/admin/login">
+            <Login />
+          </Route>
+          <Route path="/admin">
+            <Redirect to="/admin/login"></Redirect>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
