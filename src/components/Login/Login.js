2175e228cc7eee43ed8c5566e7fc69560c3e5d8c
unknown <laxezice@gmail.com>
2021-08-22 14:17:55 +0700

    refactor login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,5 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 // import "./Login.css";
